53954e8bf3f05dad5634521006f073d8be037f38
Robert Susewell <susewell@live.unc.edu>
2022-03-31 19:44:20 -0400

    middleware

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 // Require Express.js
 const express = require('express')
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const logging = (req, res, next) => {
+    console.log(req.body.number)
+    next()
+}
+
+
+
 var port = 5000
 
 const server = app.listen(port, () => {
@@ -22,8 +33,16 @@ app.get('/app', (req, res) => {
 })
 
 
-app.get('/app/echo/:number', (req, res) => {
+app.get('/app/echo/:number', express.json(), (req, res) => {
     res.status(200).json({ 'message': req.params.number })
+})
+
+app.get('/app/echo/', (req, res) => {
+    res.status(200).json({ 'message' : req.query.number })
+})
+
+app.get('/app/echo/', logging, (req, res) => {
+    res.status(200).json({ 'message' : req.body.number })
 })
 
 app.get('/app/flip', (req, res) => {
