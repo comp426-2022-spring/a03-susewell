85480e574d9af789290403a8b5b3972613353e90
Robert Susewell <susewell@live.unc.edu>
2022-04-04 12:44:50 -0400

    edited 404 not found for endpoint

--- server.js
+++ server.js
@@ -58,6 +58,6 @@ app.get('/app/flip', (req, res) => {
 })
 
 app.use(function(req, res) {
-    res.status(404).send("Endpoint does not exist")
+    res.status(404).send("404 Not Found")
     res.type("text/plain")
 })
